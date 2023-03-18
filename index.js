567bc31fb880712fd1ac0bb538ba16d1bd73e34a
Melchizedek <melchizedek.maranga@gmail.com>
2023-03-18 22:30:46 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate"){
+    return(`This Saturday, I want to ${activity}!`)
+    console.log(`This Saturday, I want to ${activity}!`)
+    
+}
+saturdayFun()
+saturdayFun("bathe my dog")
+
+const mondayWork = function (mondayTask = "go to the office"){
+    return(`This Monday, I will ${mondayTask}.`)
+}
+
+function wrapAdjective(visualFlairAdjective = "*"){
+    return(function innerfunction(parameter = "special"){
+        return(`You are ${visualFlairAdjective}${parameter}${visualFlairAdjective}!`)
+    }
+)}
+wrapAdjective("*")(`a hard worker`)
